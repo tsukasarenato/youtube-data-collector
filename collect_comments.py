1ed2b894c4783b49a0249296dd5fd2d78ac577aa
unknown <tsukasa.renato@gmail.com>
2020-05-31 23:37:33 -0300

    Youtube API - nextPageToken configured

--- collect_comments.py
+++ collect_comments.py
@@ -1,53 +1,61 @@
 import googleapiclient.discovery
-import json
-
-my_api_key = 'your key'
-api_service_name = "youtube"
-api_version = "v3"
-
-youtube = googleapiclient.discovery.build(
-    api_service_name, api_version, developerKey = my_api_key)
 
 
-def collect_comments(youtube, videoId, pageToken):
+def collect_comments(video_id, page_token):
+
+    my_api_key = 'AIzaSyDF3vOONfedg_dvhyue1QsvUtDFMsF3gb0'
+    api_service_name = "youtube"
+    api_version = "v3"
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=my_api_key)
+
     results = youtube.commentThreads().list(
         part="snippet,replies",
         maxResults=100,
-        videoId=videoId,
+        videoId=video_id,
         textFormat="plainText",
-        pageToken=pageToken
+        pageToken=page_token
     ).execute()
+
+    print('100 comments returned')
+
     return results
 
 
 def write_json_file(comments, name):
+
     text = ''
-    for item in comments['items']:
-        
+
+    for item in comments:
+
         comment = item["snippet"]["topLevelComment"]["snippet"]
-        text += 'author: ' + comment['authorDisplayName'] + '\n' 
-        text += 'comment: ' + comment["textDisplay"].replace("\n", ". ") + '\n' 
+        text += 'author: ' + comment['authorDisplayName'] + '\n'
+        text += 'comment: ' + comment["textDisplay"].replace("\n", ". ") + '\n'
         text += 'date: ' + comment["updatedAt"] + '\n'
         text += 'like: ' + str(comment["likeCount"]) + '\n\n'
         if 'replies' in item.keys():
             replies = item["replies"]["comments"]
             text += '\nreplies: \n'
-            for replie in replies:
-                text += 'author: ' + replie["snippet"]['authorDisplayName'] + '\n' 
-                text += 'comment: ' + replie["snippet"]["textDisplay"].replace("\n", ". ") + '\n' 
-                text += 'date: ' + replie["snippet"]["updatedAt"] + '\n'
-                text += 'like: ' + str(replie["snippet"]["likeCount"]) + '\n\n'
-                
-        
-    with open('database/comments/'+name+'.txt', 'w', encoding="utf8") as file:
+            for reply in replies:
+                text += 'author: ' + reply["snippet"]['authorDisplayName'] + '\n'
+                text += 'comment: ' + reply["snippet"]["textDisplay"].replace("\n", ". ") + '\n'
+                text += 'date: ' + reply["snippet"]["updatedAt"] + '\n'
+                text += 'like: ' + str(reply["snippet"]["likeCount"]) + '\n\n'
+
+    with open('database/comments/' + name + '.txt', '+a', encoding="utf8") as file:
         file.write(text)
-    
+
     print('Information was stored in txt file')
+
 
 videos = ['MX82C2rQSXo']
 
-
 for video in videos:
-    comments = collect_comments(youtube, video, '')
-    write_json_file(comments, video)
-    
+    try:
+        comment_data = collect_comments(video, '')
+        while True:
+            write_json_file(comment_data['items'], video)
+            comment_data = collect_comments(video, comment_data['nextPageToken'])
+    except KeyError:
+        print('Comments successfully collected')
