7e8b2b062e548f21d97c0020255f0f237adba5e2
Renato <tsukasa.renato@gmail.com>
2021-02-17 20:28:47 -0300

    Adding code lines to check the directory exists

--- utils/util.py
+++ utils/util.py
@@ -1,3 +1,5 @@
+import os
+
 def str_clear(text):
     """
     This function removes characters that can cause problems in the JSON file.
@@ -7,3 +9,14 @@ def str_clear(text):
     text = text.replace('\\', '')
     text = text.replace('\n', ' ')
     return text.replace('"', '')
+
+def check_path(path):
+    """
+    This function creates directories if it not exists
+    """
+    
+    if not os.path.isdir(path):
+        os.makedirs(path)
+        print("Directory created")
+    else:
+        print("Directory exists")
